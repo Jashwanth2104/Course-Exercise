cabff9abaf9ab2219cb0ee1382667c0f39c56a92
Jashwanth2104 <jashwanthkadali7379@gmail.com>
2026-02-27 23:02:09 +0530

    adlb from sdtmlb

--- Data Manipulation 1 Exercises/exercise-1.R
+++ Data Manipulation 1 Exercises/exercise-1.R
@@ -4,13 +4,15 @@
 
 # Load libraries
 library(dplyr)
+library(haven)
+setwd("D:\\R\\UpdatedCDISCPilotData\\UpdatedCDISCPilotData")
 
 #-----------------------------------------------------------
 # 0. Read SDTM LB and ADSL datasets
 #-----------------------------------------------------------
 
-lb   <- read.csv("sdtm_lb.csv", stringsAsFactors = FALSE)
-adsl <- read.csv("adsl.csv", stringsAsFactors = FALSE)
+lb   <- read_xpt("SDTM/lb.xpt")
+adsl <- read_xpt("ADAM/adsl.xpt")
 
 #-----------------------------------------------------------
 # 01. Select Variables
@@ -20,7 +22,8 @@ adsl <- read.csv("adsl.csv", stringsAsFactors = FALSE)
 #-----------------------------------------------------------
 
 adlb_step1 <- lb %>%
-  # Your code here
+        select(STUDYID,USUBJID,LBTEST,LBSTRESN,LBSTRESU,LBDTC,VISITNUM)
+adlb_step1
   
   
   #-----------------------------------------------------------
@@ -31,6 +34,9 @@ adlb_step1 <- lb %>%
 
 adlb_step2 <- adlb_step1 %>%
   # Your code here
+  inner_join(filter(select(adsl,STUDYID,USUBJID,SAFFL),SAFFL == "Y"),by=c("STUDYID","USUBJID"))
+
+ #filter(if_any(adlb_step1,SAFFL == "Y", !is.na(AVAL)))
   
   
   #-----------------------------------------------------------
@@ -43,6 +49,14 @@ adlb_step2 <- adlb_step1 %>%
 
 adlb_step3 <- adlb_step2 %>%
   # Your code here
+  mutate(
+    PARAMCD=case_when(
+       LBTEST == "Alanine Aminotransferase" ~ "ALT",
+       LBTEST == "Aspartate Aminotransferase" ~ "AST",
+       LBTEST == "Bilirubin" ~ "BILI",
+       TRUE ~ NA_character_
+    )
+  )
   
   
   #-----------------------------------------------------------
@@ -53,6 +67,7 @@ adlb_step3 <- adlb_step2 %>%
 
 adlb_step4 <- adlb_step3 %>%
   # Your code here
+mutate(AVAL=LBSTRESN)
   
   
   #-----------------------------------------------------------
@@ -62,7 +77,7 @@ adlb_step4 <- adlb_step3 %>%
 
 adlb_step5 <- adlb_step4 %>%
   # Your code here
-  
+  mutate(AVISITN=VISITNUM)
   
   #-----------------------------------------------------------
 # 06. Derive ABLFL
@@ -72,6 +87,9 @@ adlb_step5 <- adlb_step4 %>%
 
 adlb_step6 <- adlb_step5 %>%
   # Your code here
+  mutate(AVAL=LBSTRESN,
+         ABLFL=if_else(VISITNUM == 0,"Y",NA_character_)
+         )
   
   
   #-----------------------------------------------------------
@@ -82,11 +100,12 @@ adlb_step6 <- adlb_step5 %>%
 
 baseline <- adlb_step6 %>%
   # Your code here
-  
+  filter(ABLFL == "Y") %>%
+  select(USUBJID,PARAMCD,BASE=AVAL)
   
   adlb_step7 <- adlb_step6 %>%
   # Your code here
-  
+  left_join(baseline,by=c("USUBJID","PARAMCD"))
   
   #-----------------------------------------------------------
 # 08. Calculate CHG
@@ -96,6 +115,7 @@ baseline <- adlb_step6 %>%
 
 adlb_final <- adlb_step7 %>%
   # Your code here
+    mutate(CHG = AVAL - BASE)
   
   
   #-----------------------------------------------------------
