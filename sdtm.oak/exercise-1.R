b6ec209fa889cab262e06c8c1b8486a849441830
Jashwanth2104 <jashwanthkadali7379@gmail.com>
2026-03-20 17:33:48 +0530

    sdtm_exercise1.R

--- sdtm.oak/exercise-1.R
+++ sdtm.oak/exercise-1.R
@@ -5,22 +5,24 @@
 # Load libraries
 library(dplyr)
 library(haven)
-
+library(sdtm.oak)
 #-----------------------------------------------------------
 # 0. Read raw demographics
 #-----------------------------------------------------------
-
-raw_dm <- read.csv("raw_demographics.csv", stringsAsFactors = FALSE)
-
+raw_dm<- pharmaverseraw::dm_raw
+ct_sdtm <- read.csv("sdtm.oak/sdtm_ct.csv")
 #-----------------------------------------------------------
 # 1. Generate oak_id_vars
 # Keep the canonical STUDYID/USUBJID pair plus any other ID
 # components (SITEID, SUBJLOC, etc.) that the pipeline expects.
 #-----------------------------------------------------------
 
-dm <- raw_dm %>%
+dm_new <- raw_dm %>%
   # Your code here (select/rename the required ID columns)
-
+  generate_oak_id_vars(
+    pat_var = "PATNUM",
+    raw_src = "DM_DOMAIN"
+  )
 
 #-----------------------------------------------------------
 # 2. Map DM variables
@@ -30,13 +32,50 @@ dm <- raw_dm %>%
 #-----------------------------------------------------------
 
   # Your code here (join raw_dm back, select and rename variables)
-
+#mapping target var(IT.AGE) to AGE in sdtm
+dm <-  
+  assign_no_ct(
+    raw_dat = dm_new,
+    raw_var = "IT.AGE",
+    tgt_var = "AGE"
+  ) %>% 
+  assign_ct(
+    raw_dat = dm_new,
+    raw_var = "IT.SEX",
+    tgt_var = "SEX",
+    ct_spec = ct_sdtm,
+    ct_clst = "C66731",
+    id_vars = oak_id_vars()
+  ) %>% 
+  assign_ct(
+    raw_dat = dm_new,
+    raw_var = "IT.ETHNIC",
+    tgt_var = "ETHNIC",
+    ct_spec = ct_sdtm,
+    ct_clst = "C66790",
+    id_vars = oak_id_vars()
+  ) %>% 
+  assign_ct(
+    raw_dat = dm_new,
+    raw_var = "IT.RACE",
+    tgt_var = "RACE",
+    ct_spec = ct_sdtm,
+    ct_clst = "C74457",
+    id_vars = oak_id_vars()
+  ) %>% 
+  assign_no_ct(
+    raw_dat = dm_new,
+    raw_var = "COUNTRY",
+    tgt_var = "COUNTRY",
+    id_vars = oak_id_vars()
+  )
+   
 
 #-----------------------------------------------------------
 # 3. Export to XPT
 # Use haven::write_xpt to write dm_oak to tree-ordered XPT.
 #-----------------------------------------------------------
-
+write_xpt(dm,"sdtm.oak/dm_ex1_output.xpt")
 # Your code here (write dm_oak to XPT)
 
 #-----------------------------------------------------------
